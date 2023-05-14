3b8caf50d804fb20bf2d33f91d394b412f6575c9
nomadicpickle <nomadicpickle@icloud.com>
2023-05-13 21:57:12 -0400

    removed next defaults

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,28 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    extend: {screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+     colors: {
+      "testcolor": "slateblue"
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
       },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
     },
   },
   plugins: [],
