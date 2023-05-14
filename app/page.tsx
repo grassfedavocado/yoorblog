9ec77015cd34a579294f7572e6eee26ace264573
nomadicpickle <nomadicpickle@icloud.com>
2023-05-13 23:43:16 -0400

    added prisma ORM & basic tailwind config

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24 bg-slate-900">
-      <h1 className="text-5xl text-teal-500 mb-16">Yoorblog Blogging Application</h1>
+    <main className="@container flex min-h-screen flex-col items-center p-24 bg-slate-900">
+      <h1 className="@lg:text-5xl @md:text-2xl @sm:text-1xl text-teal-500 mb-16">Yoorblog Blogging Application</h1>
       <p className="text-white p-18">Yoorblog is going to an alternative to websites like wordpress & medium.</p>
     </main>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
-    },
-     colors: {
-      "testcolor": "slateblue"
-    },
-    fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
-    },
     extend: {
-      spacing: {
-        '128': '32rem',
-        '144': '36rem',
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "976px",
+        xl: "1440px",
       },
-      borderRadius: {
-        '4xl': '2rem',
-      }
-    }
+      colors: {
+        testcolor: "slateblue",
+      },
+      fontFamily: {
+        sans: ["Graphik", "sans-serif"],
+        serif: ["Merriweather", "serif"],
+      },
+      extend: {
+        spacing: {
+          128: "32rem",
+          144: "36rem",
+        },
+        borderRadius: {
+          "4xl": "2rem",
+        },
+      },
     },
   },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/container-queries")],
+};
