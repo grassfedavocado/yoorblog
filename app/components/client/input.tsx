4b07c1c3490445f49b4d777e7dd7aaff2e678b09
nomadicpickle <nomadicpickle@icloud.com>
2023-05-14 22:39:03 -0400

    updated styled components

--- app/components/client/input.tsx
+++ app/components/client/input.tsx
@@ -11,7 +11,7 @@ export default function Input(props: Props) {
   return (
     <input
       type={props?.type ?? "text"}
-      className="mx-2 my-2 px-4 py-2 border border-teal-500 text-black"
+      className="m-2 px-2 py-1 text-black text-lg outline-none border-2 border-teal-500 focus:border-b-4 focus:border-r-4 focus:border-teal-600 transition duration-400"
       placeholder={props?.placeholder ?? ""}
       value={props?.value}
       onChange={props?.onChange}
