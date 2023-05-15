3ada99bfcc1056821a097073241bf625b55cf141
nomadicpickle <nomadicpickle@icloud.com>
2023-05-15 16:21:57 -0400

    fixed stylings for mobile & desktop

---
+++ components/server/body.tsx
@@ -0,0 +1,16 @@
+export default function Body() {
+  return (
+    <div className="lg:mx-10 lg:px-10 xs:mx-10 text-center">
+      <p className="text-sm md:text-lg">
+        Our blogging software is a unique platform that combines the best features of WordPress and Drupal but instead of PHP, we use TypeScript. This
+        makes our software more efficient and faster than other blogging platforms. Our software is designed to be user-friendly and easy to use, even
+        for those who are not tech-savvy. One of the key features of our software is custom theming. We understand that every blogger has their own
+        unique style and preferences when it comes to the look and feel of their blog. That’s why we offer custom theming options that allow you to
+        create a blog that truly reflects your personality and style. Our custom theming options are easy to use and require no coding knowledge.Our
+        software also offers a range of customization options that allow you to create a blog that truly reflects your personality and style. You can
+        choose from a wide range of fonts, colors, and layouts to create a unique look for your blog. We also offer a range of plugins and widgets
+        that allow you to add additional functionality to your blog.
+      </p>
+    </div>
+  );
+}
--- components/server/navbar.tsx
+++ components/server/navbar.tsx
@@ -2,11 +2,11 @@ import Button from "../client/button";
 
 export default function Navbar() {
   return (
-    <div className="mb-5 px-5 h-1/4 flex justify-between border-b-8 border-b-white">
+    <div className="mb-5 px-5 h-1/4 flex justify-center md:justify-between border-b-8 border-b-white">
       <div className="my-8">
         <h1 className="py-1 text-4xl text-teal-500 font-bold">Yoorblog</h1>
       </div>
-      <div className="my-4 xl:block lg:block md:block sm:hidden xs:hidden">
+      <div className="my-4 hidden md:block">
         <Button text="Signup" />
         <Button text="Login" />
       </div>
