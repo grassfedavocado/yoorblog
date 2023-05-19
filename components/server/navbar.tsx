579a73b0169e2d391b1912f418904f653c000085
nomadicpickle <nomadicpickle@icloud.com>
2023-05-19 12:09:21 -0400

    fixed userlogin/signup with header navbar ui fixes

--- components/server/navbar.tsx
+++ components/server/navbar.tsx
@@ -5,21 +5,25 @@ import { auth, UserButton } from "@clerk/nextjs";
 export default function Navbar() {
   const session = auth();
 
-  console.log(session);
-
   return (
-    <div className="flex justify-center border-b-8 border-b-white px-5 md:justify-between">
+    <div className="flex flex-col justify-center border-b-8 border-b-white px-5 md:flex-row md:justify-between">
       <div className="my-8">
-        <h1 className="py-1 text-4xl font-bold text-teal-500">Yoorblog</h1>
+        <h1 className="py-1 text-4xl font-bold text-teal-500 text-center md:text-start">Yoorblog</h1>
       </div>
-      <div className="my-4 hidden md:block">
-        <Link href="register">
-          <Button text="Signup" />
-        </Link>
-        <Link href="login">
-          <Button text="Login" />
-        </Link>
-      </div>
+      {session?.userId ? (
+        <div className="mb-6 flex justify-center md:m-10">
+          <UserButton afterSignOutUrl="/" />
+        </div>
+      ) : (
+        <div className="mb-3 md:my-4 text-center">
+          <Link href="register">
+            <Button text="Signup" />
+          </Link>
+          <Link href="login">
+            <Button text="Login" />
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
