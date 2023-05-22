c31ede86f8ba95ff26a031bcb38159e98ee3e76e
nomadicpickle <nomadicpickle@icloud.com>
2023-05-21 21:14:12 -0400

    updated footer text color

--- components/server/footer.tsx
+++ components/server/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="sticky bottom-0 w-full border-t-2 border-white bg-teal-500 text-center">
+    <div className="sticky bottom-0 w-full border-t-2 border-white bg-teal-500 text-center text-white">
       <h4 className="my-2 text-lg sm:text-2xl md:my-3 md:text-2xl">Yoorblog ©️ 2023</h4>
     </div>
   );
