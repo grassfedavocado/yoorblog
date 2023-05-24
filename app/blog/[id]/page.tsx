f7e174778fb74933ffb8fc6b2da8dc028f7e3e86
iimpulsivedev <nomadicpickle@icloud.com>
2023-05-24 10:06:24 -0400

    removed unused code

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,5 +1,4 @@
 import db from "@/utils/database";
-import { currentUser } from "@clerk/nextjs";
 import NavbarWithBack from "@/components/server/navbarWithBack";
 import Footer from "@/components/server/footer";
 
@@ -10,16 +9,6 @@ type Props = {
 };
 
 export default async function Blog({ params }: Props) {
-  const user = await currentUser();
-
-  if (!user) {
-    return (
-      <h1 className="text-5xl">
-        You do not have access to this resource. Please login and try again.
-      </h1>
-    );
-  }
-
   const post = await db.post.findFirst({
     where: {
       id: Number(params.id),
