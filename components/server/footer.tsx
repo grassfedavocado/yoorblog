50f997d430ae40c8c7f31a19b5670558f39b8e3c
iimpulsivedev <nomadicpickle@icloud.com>
2023-05-26 00:22:23 -0400

    more styling fixes for blogs on the front page

--- components/server/footer.tsx
+++ components/server/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="sticky bottom-0 flex w-full flex-grow flex-row justify-center border-t-2 border-white bg-teal-500 text-white">
+    <div className="sticky bottom-0 flex w-full flex-row justify-center border-t-2 border-white bg-teal-500 text-white">
       <h4 className="my-2 text-center text-lg sm:text-2xl md:text-2xl">Yoorblog ©️ 2023</h4>
     </div>
   );
--- components/server/navbarWithBack.tsx
+++ components/server/navbarWithBack.tsx
@@ -3,7 +3,7 @@ import Button from "../client/button";
 
 export default function NavbarWithBack() {
   return (
-    <div className="m-10">
+    <div className="m-5 flex flex-grow flex-col justify-center">
       <Link href="/">
         <Button text="Back" />
       </Link>
