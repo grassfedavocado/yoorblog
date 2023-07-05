a4de5f8a5484aaa3bd8ffbd794b51cf4822716fa
iimpulsivedev <noah.pickle99@outlook.com>
2023-07-05 15:27:18 -0400

    added most recent blogs for users page

--- app/blog/[username]/page.tsx
+++ app/blog/[username]/page.tsx
@@ -1,9 +1,35 @@
+import db from "@/utils/database";
+import Card from "@/components/server/Card";
+
 type Props = {
   params: {
     username: string;
   };
 };
 
-export default function page({ params }: Props) {
-  return <div>Most recent Blogs From user: {params.username}!</div>;
+export default async function Blogs(props: Props) {
+  const blogs = await db.post.findMany({
+    where: {
+      author: props.params.username,
+    },
+    orderBy: {
+      created_at: "desc",
+    },
+    take: 20,
+  });
+
+  return (
+    <div className="my-5 flex w-full flex-col items-center justify-center pb-6 md:flex-row md:flex-wrap md:items-start">
+      {blogs?.map((blog, index) => {
+        return (
+          <Card
+            key={index}
+            title={blog.title}
+            author={blog.author}
+            link={`/blog/${blog.author}/${blog.slug}`}
+          />
+        );
+      })}
+    </div>
+  );
 }
