8ed855ac7a65303e4d5eda4cfd2eb2b0fece753c
iimpulsivedev <noah.pickle99@outlook.com>
2023-07-07 14:30:49 -0400

    added github icon with link to project

--- components/server/footer.tsx
+++ components/server/footer.tsx
@@ -1,8 +1,27 @@
+import Link from "next/link";
+import Image from "next/image";
+import Github from "@/public/github.svg";
+
 export default function Footer() {
   return (
-    <div className="w-min-screen sticky bottom-0 border-t-2 border-black bg-white">
-      <p className="m-3 text-center font-bold text-lg md:m-5 md:text-2xl">
-        Yoorblog ©️ 2023 <span className="font-normal">Made with 💙, Open Source.</span>
+    <div className="w-min-screen border-t-2 border-black bg-white">
+      <p className="m-3 text-center text-md md:m-5 md:text-2xl">
+        Yoorblog ©️ 2023{" "}
+        <span className="font-normal">
+          Made with 💙, Open Source{" "}
+          <Link
+            href="https://github.com/noahpickle/yoorblog"
+            target="_blank"
+          >
+            <Image
+              src={Github}
+              alt="github icon"
+              width={36}
+              height={36}
+              className="inline"
+            />
+          </Link>
+        </span>
       </p>
     </div>
   );
